161be49391535ac31719b480516fbfea5b490b0f
Zhuoqing <wuzhuoqing@hotmail.com>
2018-12-06 22:13:20 -0800

    add firsttech download hack

--- ofx_pythondownload.py
+++ ofx_pythondownload.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python
 import sys, os
-#import requests
 import httplib, ssl
 from collections import OrderedDict
+from shutil import copyfile
+from datetime import datetime
 import logging
 
 logfileName = os.path.abspath(sys.argv[0]) + '.log';
 logging.basicConfig(filename = logfileName, level = logging.WARNING)
 #logging.basicConfig(filename = logfileName, level = logging.DEBUG)
 
+logging.info('direct python get starting')
+
 method = sys.argv[1]
 url = sys.argv[2];
 metaFile = sys.argv[3];
 postFile = sys.argv[4];
 dstFile = sys.argv[5];
+
+#copyfile(metaFile, os.path.abspath(sys.argv[0]) + '.metaFile.txt')
+#copyfile(postFile, os.path.abspath(sys.argv[0]) + '.postFile.txt')
 
 logging.info(str(sys.argv))
 try:
@@ -23,21 +29,6 @@ try:
 		content_bytes = in_file.read()
 		in_file.close()
 	
-		#od = OrderedDict()
-		#od['Content-Type'] = 'application/x-ofx'
-		#od['Host'] = 'ofx.discovercard.com'
-		#od['Content-Length'] = str(len(content_bytes))
-		#od['Connection'] = 'Keep-Alive'
-		#s = requests.Session()
-		#s.headers = od
-		# remove default headers for some reason 'Accept-Encoding': None doesnt work
-		#req = requests.Request(method, url, headers = {'Accept': None, 'User-Agent': None}, data = content_bytes)
-		#prepped = s.prepare_request(req)
-		#logging.info('prepared headers %s', resp.request.headers)
-		#del prepped.headers['Accept-Encoding']
-		#resp = s.send(prepped, verify = False)
-		#logging.info('request headers %s', resp.request.headers)
-
 		conn = httplib.HTTPSConnection("ofx.discovercard.com")#, context = ssl._create_unverified_context())
 		#conn.set_debuglevel(1)
 		conn.connect()
@@ -54,6 +45,34 @@ try:
 		out_file = open(dstFile, "w")
 		out_file.write(respBody)
 		out_file.close()
+		sys.exit(0)
+
+	if url == 'https://ofx.firsttechfed.com':
+		logging.info('direct python get for firsttechfed')
+		in_file = open(postFile, "r")
+		content_bytes = in_file.read()
+		in_file.close()
+	
+		conn = httplib.HTTPSConnection("ofx.firsttechfed.com")#, context = ssl._create_unverified_context())
+		conn.set_debuglevel(5)
+		conn.connect()
+		conn.putrequest(method, 'https://ofx.firsttechfed.com/', skip_host = True, skip_accept_encoding = True)
+		conn.putheader('Accept', '*/*')
+		conn.putheader('User-Agent', 'InetClntApp/3.0')
+		conn.putheader('Date', datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'))
+		conn.putheader('Content-Type', 'application/x-ofx')
+		conn.putheader('Host', 'ofx.firsttechfed.com')
+		conn.putheader('Content-Length', str(len(content_bytes)))
+		conn.putheader('Connection', 'Keep-Alive')
+		conn.endheaders()
+		conn.send(content_bytes)
+		response = conn.getresponse()
+		respBody = response.read()
+
+		out_file = open(dstFile, "w")
+		out_file.write(respBody)
+		out_file.close()
+		#copyfile(dstFile, os.path.abspath(sys.argv[0]) + '.resFile.txt')
 		sys.exit(0)
 
 except Exception as e:
